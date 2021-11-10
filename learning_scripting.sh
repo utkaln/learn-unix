b283db1140228d45e91b8b74041e5b51a1a90269
Utkal Nayak <nayak.utkal@gmail.com>
2021-11-09 19:52:27 -0500

    added external param as var

--- learning_scripting.sh
+++ learning_scripting.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+clear
 var_name=SampleFileName
 echo "Today is November 8, 2021. And the config file used is --> $var_name "
 
@@ -11,4 +12,19 @@ else
     echo "No directory Found. Exiting..."
 fi
 
+echo "***************************"
+param_first=$1
+param_second=$2
+
+if [ "$param_first" == "abcd" ]
+then
+    echo "Inside first if block --> $param_first"
+elif [ "$param_second" == "abcd" ]
+then 
+    echo "Inside el if with second param --> $param_second"
+else
+    echo "In the else block"
+fi
+
+echo "***************************"
 
