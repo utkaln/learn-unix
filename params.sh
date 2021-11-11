b6778f9e871d3fd7dce80bff7e7078f44fe5d4f8
Utkal Nayak <nayak.utkal@gmail.com>
2021-11-11 00:07:55 -0500

    added functions

--- params.sh
+++ params.sh
@@ -1,25 +1,55 @@
 #! /bin/bash
-echo "Start --> ***************"
-echo "Starting Params script to read user input"
+echo "*************** | --> "
+function useInputParams {
+    echo "Starting Params script to read user input"
+    read -p " Please enter your password: " user_pwd
+    # assign the user input to variable user_pwd
+    echo " You entered value --> $user_pwd "
+    echo " Number of params sent = $# "
+    echo " Value of all the params entered: $*"
+}
 
-read -p " Please enter your password: " user_pwd
-echo " You entered value --> $user_pwd "
-echo " Number of params sent = $# "
-echo " Value of all the params entered: $*"
 
 # loop through parameters
-for param in $*
+function useFor {
+    for param in $*
     do
         echo "$param"
     done
+}
 
 #  While loop
-sum=0
-while [ "$sum" -lt 1000 ]
-do
-    read -p "enter a score: " score
-    sum=$(($sum+$score))
-    echo "Score now is : $sum"
-done
+function useWhile {
+    sum=0
+    while [ "$sum" -lt 1000 ]
+    do
+        read -p "enter a score: " score
+        sum=$(($sum+$score))
+        echo "Score now is : $sum"
+    done
+}
 
-echo "*************** --> Finish"
+function createFile() {
+    touch $1
+    if [ $2 == true ]
+    then
+        chmod 744 $1
+    else
+        chmod 700 $1
+    fi
+}
+
+function listFiles {
+    echo "List of files in this directory --> "
+    ls -la
+}
+
+
+# to execute invoke a function name from above
+createFile utkal_test.sh true 
+listFiles
+
+createFile utkal_test.txt false
+listFiles
+
+echo "*************** --> | "
