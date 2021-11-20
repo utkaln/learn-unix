74c934a3cfc48cfe9ee0a3ac20468980b4954998
Utkal Nayak <nayak.utkal@gmail.com>
2021-11-20 17:58:54 -0500

    separated echo lines

--- simple-jenkins.sh
+++ simple-jenkins.sh
@@ -1,5 +1,6 @@
 #! /bin/bash 
 echo "Starting simple-jenkins.sh execution --> "
 var_name=$(ls /var/log)
-echo "file list is $var_name"
-echo "Finish simple-jenkins.sh execution || "
+echo "file list is --> "
+echo "$var_name"
+echo "Finished simple-jenkins.sh execution || "
