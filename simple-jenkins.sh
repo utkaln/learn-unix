4d1d953a7fdd8ea4f368e5ca2bcd1c011e0102d7
Utkal Nayak <nayak.utkal@gmail.com>
2021-11-20 18:16:43 -0500

    Update simple-jenkins.sh

--- simple-jenkins.sh
+++ simple-jenkins.sh
@@ -3,4 +3,4 @@ echo "Starting simple-jenkins.sh execution --> "
 var_name=$(ls /var/log)
 echo "file list is --> "
 echo "$var_name"
-echo "Finished simple-jenkins.sh execution || "
+echo "Finished simple-jenkins.sh execution. (Remote) "
